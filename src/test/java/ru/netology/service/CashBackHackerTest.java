09bc435d558e7750efc8ead5ce06a95c942a849b
Texnolog24 <latkina0808@yandex.ru>
2025-05-07 13:16:11 +0500

    Написали тесты

--- src/test/java/ru/netology/service/CashBackHackerTest.java
+++ src/test/java/ru/netology/service/CashBackHackerTest.java
@@ -7,6 +7,38 @@ import static org.junit.jupiter.api.Assertions.*;
 class CashBackHackerTest {
 
     @Test
-    void remain() {
+    void testRemainWhenAmountLessThanBoundary() {
+
+        CashBackHacker cashBackHacker = new CashBackHacker();
+        int amount = 800;
+        int expectedResult = 200;
+
+        int actualResult = cashBackHacker.remain(amount);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testRemainWhenAmountIsMultipleOfBoundary() {
+
+        CashBackHacker cashBackHacker = new CashBackHacker();
+        int amount = 1000;
+        int expectedResult = 0;
+
+        int actualResult = cashBackHacker.remain(amount);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testRemainWhenAmountGreaterThanBoundary() {
+
+        CashBackHacker cashBackHacker = new CashBackHacker();
+        int amount = 1700;
+        int expectedResult = 300;
+
+        int actualResult = cashBackHacker.remain(amount);
+
+        assertEquals(expectedResult, actualResult);
     }
 }
